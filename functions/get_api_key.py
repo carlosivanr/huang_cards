238f6b68c4ae49b2fbbc828799065da325f7ce7f
rodrica2 <carlos.i.rodriguez@cuanschutz.edu>
2024-11-15 15:13:04 -0700

    Updated get_api_key function, report.qmd, and requirements.txt

--- functions/get_api_key.py
+++ functions/get_api_key.py
@@ -1,6 +1,8 @@
 # %%
 def get_api_key(project_id):
-    """Get API Key. Returns the RedCap API Key/Token from a credentials file according to a RedCap Project ID.
+    """
+    Get API Key. Returns the RedCap API Key/Token from a credentials file according to a RedCap Project ID.
+    Set the creds file to the credentials file containing all of the RedCap Project urls and tokens
     """
     import pandas as pd
     creds = pd.read_csv("C:/Users/rodrica2/OneDrive - The University of Colorado Denver/Documents/redcap_credentials/credentials")
